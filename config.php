918fc7f41328e00415929d7de6b788b01426476e
Paolo dalex <paolodalex96@gmail.com>
2022-09-05 16:24:09 +0200

    fix: edit config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	define('DB_SERVER', '127.0.0.1');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_DATABASE', 'socialyze');
+	define('DB_USERNAME', 'u819772339_root');
+	define('DB_PASSWORD', 'P(1@m87D69C&G');
+	define('DB_DATABASE', 'u819772339_socialyze');
 	$con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 	if (!$con) {
 	die("Connection failed: " . mysqli_connect_error());
